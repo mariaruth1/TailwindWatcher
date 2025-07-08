a98fea2de01609964a623506223bbc4cae1d9f38
Maria Ruth Nielsen <mni@sik.dk>
2025-07-08 11:53:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+﻿# Ruthless.TailwindWatcher
+
+A simple .NET NuGet package designed for **debug and development environments only** to automatically watch and rebuild your Tailwind CSS files using the Tailwind CSS CLI.
+
+## Installation
+
+Install the NuGet package into your .NET project:
+
+dotnet add package Ruthless.TailwindWatcher --version 1.0.0
+
+## Running Tailwind styling
+
+Download tailwind v4.X.X executable: https://github.com/tailwindlabs/tailwindcss/releases
+
+In Program.cs add:
+
+#if DEBUG
+using TailwindWatcher;
+// Example Usage:
+TailwindUtils.WatchTailwind(
+    tailwindFullPath: "C:\\YourProject\\tailwind-cli\\tailwindcss.exe", // Full path to your tailwind executable
+    inputCssPath: "Styles/tailwind-app.css", // Path to your input CSS from project root
+    outputCssPath: "wwwroot/css/tailwind-app.css" // Path to your output CSS in wwwroot
+#endif
+
+If in debug mode tailwind will now build and run automatically
---
+++ TailwindUtils.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace TailwindWatcher
+{
+    public class TailwindUtils
+    {
+        /// <summary>
+        /// Starts a process that watches for changes and rebuilds accordingly
+        /// </summary>
+        /// <param name="tailwindFullPath">The full path to your tailwind executeable</param>
+        /// <param name="inputCssPath">The path to your input css from project root</param>
+        /// <param name="outputCssPath">The path to your output css from wwwroot</param>
+        public static void WatchTailwind(string tailwindFullPath, string inputCssPath, string outputCssPath)
+        {
+            try
+            {
+                var process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = tailwindFullPath,
+                        Arguments = $"-i {inputCssPath} -o {outputCssPath}  --watch",
+                        WindowStyle = ProcessWindowStyle.Normal,
+                        RedirectStandardError = true,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                    }
+                };
+
+                bool started = process.Start();
+
+                if (started)
+                {
+                    Console.WriteLine("Started TailwindCSS in watch-mode...");
+                    process.WaitForExitAsync();
+                }
+                else
+                {
+                    Console.WriteLine("Failed to start TailwindCSS!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error starting TailwindCSS: {ex.Message}");
+            }
+        }
+    }
+}
